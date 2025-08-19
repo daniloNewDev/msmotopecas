49e29d1578c47bad239db918e49ce294f29ee708
Danilo Leite <100138146+daniloNewDev@users.noreply.github.com>
2025-08-19 13:50:26 -0300

    fixing footer

--- src/components/InfoSection.tsx
+++ src/components/InfoSection.tsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react"
 import gifmoto2 from "../assets/gifmoto2.gif"
 import gifmoto3 from "../assets/gifmoto3.gif"
+import Footer from "./Footer";
 
 const InfoSection = () => {
   const gifs = [gifmoto2, gifmoto3];
@@ -16,8 +17,8 @@ const InfoSection = () => {
   return (
     <div className="w-full h-full flex flex-col items-center justify-center bg-gray-900">
       <h1 className="text-white mt-14 mb-14">MS Moto Peças</h1>
-      <img src={gifs[current]} alt="" className="w-full mb-22" />
-
+      <img src={gifs[current]} alt="" className="w-full mb-14" />
+      <Footer />
     </div>
   )
 }
