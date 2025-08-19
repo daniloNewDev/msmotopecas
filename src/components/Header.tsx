ec2c65d274ca0ec27500d6902b99b1a300aa1978
Danilo Leite <100138146+daniloNewDev@users.noreply.github.com>
2025-08-19 12:40:51 -0300

    fixing nav layout

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -9,13 +9,13 @@ const Header = () => {
   };
 
   return (
-    <header className="w-full h-16 bg-red-700 flex items-center justify-between">
+    <header className="w-full h-16 bg-red-700 flex justify-between">
       <Link to={"/"}>
         <img src="mslogo.svg" className="ml-2 mt-2" />
       </Link>
-      <h1 className='text-white font-semibold dark:text-white'>MS Moto Peças</h1>
+      <h1 className='text-white font-semibold mt-5 dark:text-white'>MS Moto Peças</h1>
       <div className="block sm:hidden">
-        <button onClick={toggleMenu} className="text-white focus:outline-none">
+        <button onClick={toggleMenu} className="text-white mt-4 focus:outline-none">
           <svg className="h-8 w-12 rounded-md fill-current" viewBox="0 0 24 24">
             <path
               fillRule="evenodd"
@@ -26,13 +26,13 @@ const Header = () => {
         </button>
       </div>
 
-      <nav className={`w-[96%] mt-68 ml-[2%] text-[#d4d4d4] font-semibold absolute flex flex-col bg-red-600 rounded-md items-start justify-start ${isOpen ? 'block' : 'hidden'}`}>
-        <Link className='w-full p-1 rounded hover:bg-red-500 text-center' to="/searchplate" onClick={() => setIsOpen(false)}>BUSCAR PLACA</Link>
-        <Link className='w-full p-1 rounded hover:bg-red-500 text-center' to="/newserviceorder" onClick={() => setIsOpen(false)}>NOVA ORDEM DE SERVIÇO</Link>
-        <Link className='w-full p-1 rounded hover:bg-red-500 text-center' to="/contacts">O.S. EM ABERTO</Link>
-        <Link className='w-full p-1 rounded hover:bg-red-500 text-center' to="/storage">ESTOQUE</Link>
-        <Link className='w-full p-1 rounded hover:bg-red-500 text-center' to="/cleaner">LAVA RÁPIDO</Link>
-        <Link className='w-full p-1 rounded hover:bg-red-500 text-center' to="/account">CAIXA</Link>
+      <nav className={`w-[96%] h-[85%] mt-18 ml-[2%] text-[#d4d4d4] font-semibold absolute flex flex-col bg-red-600 rounded-md items-start justify-start ${isOpen ? 'block' : 'hidden'}`}>
+        <Link className='w-full p-3 text-2xl rounded hover:bg-red-500 text-center' to="/searchplate" onClick={() => setIsOpen(false)}>BUSCAR PLACA</Link>
+        <Link className='w-full p-3 text-2xl rounded hover:bg-red-500 text-center' to="/newserviceorder" onClick={() => setIsOpen(false)}>NOVA ORDEM DE SERVIÇO</Link>
+        <Link className='w-full p-3 text-2xl rounded hover:bg-red-500 text-center' to="/contacts">O.S. EM ABERTO</Link>
+        <Link className='w-full p-3 text-2xl rounded hover:bg-red-500 text-center' to="/storage">ESTOQUE</Link>
+        <Link className='w-full p-3 text-2xl rounded hover:bg-red-500 text-center' to="/cleaner">LAVA RÁPIDO</Link>
+        <Link className='w-full p-3 text-2xl rounded hover:bg-red-500 text-center' to="/account">CAIXA</Link>
       </nav>
     </header>
   )
