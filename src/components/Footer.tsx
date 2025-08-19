caf29d34a187a1f237b1f2959fc5d713a3c85b1e
Danilo Leite <100138146+daniloNewDev@users.noreply.github.com>
2025-08-19 13:37:24 -0300

    add footer and gifs

---
+++ src/components/Footer.tsx
@@ -0,0 +1,40 @@
+import logoInsta from "../assets/logoinsta.png"
+import msLogo from "../assets/logo-ms.png"
+import whatsLogo from "../assets/whatsapp.png"
+import mailLogo from "../assets/maillogo.png"
+
+const Footer = () => {
+  return (
+    <footer className='bg-red-700 w-full mt-22 py-8 flex flex-col gap-12 items-center justify-around'>
+      <img src={msLogo} className='w-28' />
+      <div>
+        <h3 className='squada-one-regular text-3xl text-white text-center'>Contatos</h3>
+        <a href="https://www.instagram.com/ms__motopecas?igsh=MW1zaXdnbGc2Ym5yag==" target="_blank" rel="noopener noreferrer">
+          <div className='flex items-center justify-start my-2'>
+            <img src={logoInsta} className='w-10' />
+            <h4 className='text-white squada-one-regular text-2xl ml-4'>@msmotopecas</h4>
+          </div>
+        </a>
+        <a href="tel:+5511981356115" target="_blank" rel="noopener noreferrer">
+          <div className='flex items-center justify-start my-2'>
+            <img src={whatsLogo} className='w-10' />
+            <h4 className='text-white squada-one-regular text-2xl ml-4'>(11) 9 8135-6115</h4>
+          </div>
+        </a>
+        <a href="mailto:msmotopecas@gmail.com">
+          <div className='flex items-center'>
+            <img src={mailLogo} className='w-10' />
+            <h4 className='text-white squada-one-regular text-2xl ml-4'>msmotopecas@gmail.com</h4>
+          </div>
+        </a>
+      </div>
+      <div>
+        <h3 className='squada-one-regular text-3xl text-white text-center'>Empresa</h3>
+        <h4 className='text-[#cfcfcf] font-sans text-md ml-4 my-4'>Todos direitos reservados a MS Moto Peças</h4>
+        <h4 className='text-[#cfcfcf] squada-one-regular text-center text-md ml-4 my-4'>MSMotoPecas &trade;</h4>
+      </div>
+    </footer>
+  )
+}
+
+export default Footer
--- src/components/InfoSection.tsx
+++ src/components/InfoSection.tsx
@@ -1,7 +1,23 @@
+import { useEffect, useState } from "react"
+import gifmoto2 from "../assets/gifmoto2.gif"
+import gifmoto3 from "../assets/gifmoto3.gif"
+
 const InfoSection = () => {
+  const gifs = [gifmoto2, gifmoto3];
+  const [current, setCurrent] = useState(0)
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCurrent((prev) => (prev + 1) % gifs.length)
+    }, 2000);
+    return () => clearInterval(interval)
+  })
+
   return (
-    <div className="w-full h-full bg-slate-900 dark:bg-white">
-      <h1 className="dark:bg-white">MS Moto Peças</h1>
+    <div className="w-full h-full flex flex-col items-center justify-center bg-gray-900">
+      <h1 className="text-white mt-14 mb-14">MS Moto Peças</h1>
+      <img src={gifs[current]} alt="" className="w-full mb-22" />
+
     </div>
   )
 }
